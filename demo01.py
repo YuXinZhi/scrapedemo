febfe61e5aa7d7a36c294aeec7391cd8792c7f8b
Ben <whoyumengmeng@gmail.com>
2017-07-31 23:35:42 +0800

    init

---
+++ demo01.py
@@ -0,0 +1,19 @@
+from urllib import request
+import random
+
+url = 'http://www.baidu.com'
+
+ua_list = [
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv2.0.1) Gecko/20100101 Firefox/4.0.1",
+        "Mozilla/5.0 (Windows NT 6.1; rv2.0.1) Gecko/20100101 Firefox/4.0.1",
+        "Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; en) Presto/2.8.131 Version/11.11",
+        "Opera/9.80 (Windows NT 6.1; U; en) Presto/2.8.131 Version/11.11",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_0) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11"
+]
+
+
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.109 Safari/537.36'}
+req = request.Request(url=url,headers=headers)
+response = request.urlopen(req)
+html = response.read()
+print(html)
